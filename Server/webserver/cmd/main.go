87ae3b704478469aa3a3ba59469f78355487d22a
Timotej979 <timotej.pet@gmail.com>
2023-03-04 21:46:36 +0100

    Fixing circular imports part 2

--- Server/webserver/cmd/main.go
+++ Server/webserver/cmd/main.go
@@ -3,15 +3,14 @@ package main
 import (
     "log"
 
-    "github.com/Timotej979/Automatic-virtual-meassurement-systems/Server/webserver/pkg/config"
-    "github.com/Timotej979/Automatic-virtual-meassurement-systems/Server/webserver/pkg/db"
+    "github.com/Timotej979/Automatic-virtual-meassurement-systems/Server/webserver/pkg/db_init"
     "github.com/Timotej979/Automatic-virtual-meassurement-systems/Server/webserver/pkg/methods"
 
     "github.com/gofiber/fiber/v2"
 )
 
 func main() {
-    c, err := config.LoadConfig()
+    c, err := db.LoadConfig()
 
     if err != nil {
         log.Fatalln("Failed at config", err)
