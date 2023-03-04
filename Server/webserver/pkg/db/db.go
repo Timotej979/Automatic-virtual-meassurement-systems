4f3f677817b41c30380524e803cc838961a6a027
Timotej979 <timotej.pet@gmail.com>
2023-03-04 21:41:32 +0100

    Removing circular imports

--- Server/webserver/pkg/db/db.go
+++ Server/webserver/pkg/db/db.go
@@ -4,13 +4,68 @@ import (
     "fmt"
     "log"
 
-    "github.com/Timotej979/Automatic-virtual-meassurement-systems/Server/webserver/pkg/config"
-    "github.com/Timotej979/Automatic-virtual-meassurement-systems/Server/webserver/pkg/models"
+    "github.com/spf13/viper"
+    "github.com/shopspring/decimal"
 
     "gorm.io/driver/postgres"
     "gorm.io/gorm"
 )
 
+////////////////////// DB MODEL DEINITIONS //////////////////////
+type AirTemperature struct {
+    ID    int    `gorm:"primaryKey"`
+	Value decimal.Decimal `gorm:"type:decimal(7,6);"`
+	Timestamp time.Time	
+}
+
+type AirHumidity struct {
+	ID    int    `gorm:"primaryKey"`
+	Value decimal.Decimal `gorm:"type:decimal(7,6);"`
+	Timestamp time.Time	
+}
+
+type SoilHumidity struct {
+	ID    int    `gorm:"primaryKey"`
+	Value decimal.Decimal `gorm:"type:decimal(7,6);"`
+	Timestamp time.Time	
+}
+
+type RelayState struct {
+	ID    int    `gorm:"primaryKey"`
+	Value int
+	Timestamp time.Time	
+}
+
+
+////////////////////// DB CONFIG //////////////////////
+type Config struct {
+    Port   string `mapstructure:"PORT"`
+    DBHost string `mapstructure:"DB_HOST"`
+    DBUser string `mapstructure:"DB_USER"`
+    DBPass string `mapstructure:"DB_PASS"`
+    DBName string `mapstructure:"DB_NAME"`
+    DBPort string `mapstructure:"DB_PORT"`
+}
+
+func LoadConfig() (c Config, err error) {
+    viper.AddConfigPath("./")
+    viper.SetConfigName(".env")
+    viper.SetConfigType("env")
+    viper.AutomaticEnv()
+
+    err = viper.ReadInConfig()
+
+    if err != nil {
+        return
+    }
+
+    err = viper.Unmarshal(&c)
+
+    return
+}
+
+
+////////////////////// DB INIT //////////////////////
 func Init(c *config.Config) *gorm.DB {
     url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", c.DBUser, c.DBPass, c.DBHost, c.DBPort, c.DBName)
     db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
