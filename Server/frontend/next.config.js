b57810f61ef1ab93b32172c66b78e30961403e41
Timotej979 <timotej.pet@gmail.com>
2023-04-14 10:12:02 +0200

    Reachable index.tsx through localhost:5001 (nginx)

--- Server/frontend/next.config.js
+++ Server/frontend/next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
 module.exports = {
-  reactStrictMode: true,
   output: 'standalone',
-  // if you have enabled gzip on nginx disable compress to offload Node.js process
-  compress: false,
-};
-
-module.exports = nextConfig
+}
