51e1a0f76baa3dd62086145a1f99fcba645b7abd
Timotej979 <timotej.pet@gmail.com>
2023-04-28 22:03:09 +0200

    Set actual physical values of the sensor pins

--- Raspberry-pi/api/rpi_dal.py
+++ Raspberry-pi/api/rpi_dal.py
@@ -30,7 +30,7 @@ class RPI_dal():
             # Create the MCP3008 object
             self.mcp = MCP.MCP3008(self.spi, self.cs)
             # Create an analog input channel on pin 0
-            self.soil_moisture_chan = AnalogIn(self.mcp, getattr(MCP, self.SOIL_MOISTURE_CS_PIN))
+            self.soil_moisture_chan = AnalogIn(self.mcp, getattr(MCP, getattr(MCP, self.SOIL_MOISTURE_CS_PIN)))
 
         # Initialize relay
         self.RELAY_PIN = os.getenv('RELAY_PIN')
